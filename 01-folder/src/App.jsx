871472860e617f0f1c6c19b3a17d85d17f146090
AlishMaga <magajualish12345@gmail.com>
2025-10-14 07:03:55 +0545

    first commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <>
+    <h1>this is div one actual coding. this is it now.</h1>
+    <h2>this is h2.</h2>
+    <div>
+      <h1>this is dive inisdid tow.</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
