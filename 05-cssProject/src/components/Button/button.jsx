7fd40fe58c4cce516512a140b32cace136ef7a3a
AlishMaga <magajualish12345@gmail.com>
2025-10-16 08:17:40 +0545

    How to add CSS in Reacts components.

---
+++ 05-cssProject/src/components/Button/button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './Button.module.css'
+const Button = () => {
+  return (
+    
+      <button className={styles.btn}>this is simple button</button>
+    
+  )
+}
+
+export default Button
